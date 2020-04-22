db6c6e02edb1e9fbcd6648fc00eee2c2ae8bd698
stephane65133 <stephanejiofatamafo@gmail.com>
2020-04-22 12:48:02 +0100

    initialcommit

---
+++ App/database/migrations/2020_04_21_233633_role_permission.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RolePermission extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('roleId')->index();
+            $table->unsignedInteger('permissionId')->index();
+            $table->boolean('isActive');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_permission');
+    }
+}
